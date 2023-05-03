d4260c891280717e8078de91c50da75d27eaf4ab
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-05-04 00:39:47 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+print("olá pessoal")
+
+import numpy as np
+
+# Carrega os dados do arquivo .npy em um array do NumPy
+array = np.load('ef654fbf03dc41b2afae039de855c6a4.npy')
+
+# Agora você pode trabalhar com o array normalmente
+print(array)
